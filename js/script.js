f057240e1928092dd33b18a9f1916827871795fb
Fabinho Brum <122577434+fabinhobrum@users.noreply.github.com>
2023-07-16 23:21:53 -0400

    Configurações de Media Query
    Iniciando a configuração da responsividade do site.
    Foi feito ajustes no formulário de contato.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var hamburguer = document.querySelector(".hamburguer");
-
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
+});
+
+
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000', {placeholder: '(__) _____-____'})
 });
