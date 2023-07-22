4e57ba572f99f9ad7fbf867c467155f0299ae2db
Fabinho Brum <122577434+fabinhobrum@users.noreply.github.com>
2023-07-22 18:23:33 -0400

    :package: Formulário de contato
    Configurando o formulário de contato para retornar de forma automática a pagina home depois de exibir que os dados foram cadastrados com sucesso.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ hamburguer.addEventListener("click", function(){
 $(document).ready(function() {
     $('#telefone').mask('(00) 00000-0000', {placeholder: '(__) _____-____'})
 });
+
+function redirecionar() {
+setTimeout(function() { 
+    window.location.href = "http://127.0.0.1:5500/index.html";
+}, 5000);
+}
