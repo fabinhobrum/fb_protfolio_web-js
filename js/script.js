005701f2cec7716730d574c21ab450c1c694bbda
Fabinho Brum <122577434+fabinhobrum@users.noreply.github.com>
2023-07-22 22:21:40 -0400

    :package: Formulario de contato
    Configurando a responsividade para varias telas

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ $(document).ready(function() {
     $('#telefone').mask('(00) 00000-0000', {placeholder: '(__) _____-____'})
 });
 
+
 function redirecionar() {
-setTimeout(function() { 
-    window.location.href = "http://127.0.0.1:5500/index.html";
-}, 5000);
+    setTimeout(function() { 
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    }, 6000);
 }
